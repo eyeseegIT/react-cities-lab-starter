a74574e82d60f660e077166d6f29e45116351667
Angela Quach <angelayunquach@gmail.com>
2021-12-05 22:28:31 -0500

    finish lab with bonus

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 // IMPORT useState
 import React, {useState} from "react";
 import "./styles.css";
-import imagesArr from "./imageData"
 // IMPORT imageData.js AND STORE IT IN A VARIABLE CALLED imagesArr
+import imagesArr from "./imageData"
 
 export default function App() {
-
-const [bigImage, setBigImage] = useState (imagesArr[0].img)
   // USE useState TO CREATE  [bigImage, setBigImage]
   // AND SET IT TO THE IMAGE URL OF THE FIRST ELEMENT IN THE ARRAY
+  const [bigImage, setBigImage] = useState (imagesArr[0].img)
 
   // CREATE A HANDLE CLICK FUNCTION THAT ACCEPTS AN IMAGE URL
   // THE FUNCTION SHOULD CALL setBigImage AND PASS IT THE URL
+  const [imageClick, setImageClick] = useState([])
 
   const handleImage = (image) => {
     setBigImage(image)
+    setImageClick([
+      ...imageClick, 
+      {image},
+    ])
   }
 
   // CREATE A VARIABLE CALLED images THAT LOOPs OVER THE imagesArr AND RETURNS AN <IMG> ELEMENT
@@ -23,6 +27,7 @@ const [bigImage, setBigImage] = useState (imagesArr[0].img)
   let images = imagesArr.map((ele, index) => {
     return (
       <img
+      style={{ border: bigImage === ele.img ? "3px solid green" : "" }}
       src={ele.img}
       alt={ele.city}
       key={index}
@@ -36,9 +41,7 @@ const [bigImage, setBigImage] = useState (imagesArr[0].img)
     <div className="App">
       <h1>Cities Of The World</h1>
       <div id="wrapper">
-        
-        <div  id="thumbnails">{images}</div>
-  
+        <div id="thumbnails" >{images}</div>
         <img src={bigImage} id="bigimage" alt='bigImage'/>
       </div>
     </div>
